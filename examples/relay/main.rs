46e62ad9c6f011611e27113784b5d5aabd2a215d
Karl Southern <karl@theangryangel.co.uk>
2021-09-19 10:35:04 +0100

    fiddling before dinner

--- examples/relay/main.rs
+++ examples/relay/main.rs
@@ -3,7 +3,7 @@ extern crate insim;
 #[tokio::main]
 pub async fn main() {
     let mut client =
-        insim::Client::connect("insim.rs".to_string(), "isrelay.lfs.net:47474".to_string()).await;
+        insim::Client::new("insim.rs".to_string(), "isrelay.lfs.net:47474".to_string()).await;
 
     let hlr = insim::Packets::RelayHostListRequest { reqi: 0 };
 
@@ -19,10 +19,14 @@ pub async fn main() {
 
     client.send(hs).await;
 
+    client.run().await;
+
+    /*
     while let Some(result) = client.recv().await {
         match result {
             Err(e) => println!("{:?}", e),
             _ => (),
         }
     }
+    */
 }
--- src/client.rs
+++ src/client.rs
@@ -2,14 +2,22 @@ use crate::{codec, proto};
 use futures::prelude::*;
 use tokio::net::TcpStream;
 use tokio_util::codec::Framed;
+use tokio::time;
+use std::time::Duration;
 
 pub struct Client {
     name: String,
     inner: Framed<TcpStream, codec::InsimCodec>,
+    timeout: time::Instant,
+}
+
+fn next_timeout() -> time::Instant {
+    time::Instant::now() + Duration::from_secs(30)
 }
 
 impl Client {
-    pub async fn connect(name: String, dest: String) -> Client {
+
+    pub async fn new(name: String, dest: String) -> Client {
         let stream = TcpStream::connect(dest).await.unwrap();
 
         let inner = Framed::new(stream, codec::InsimCodec::new());
@@ -17,12 +25,13 @@ impl Client {
         let mut client = Client {
             name: name.to_owned(),
             inner,
+            timeout: next_timeout(),
         };
         client.init().await;
         client
     }
 
-    pub async fn init(&mut self) {
+    async fn init(&mut self) {
         let isi = proto::Insim::Init {
             name: self.name.to_owned(),
             password: "".to_string(),
@@ -36,28 +45,59 @@ impl Client {
         };
 
         self.send(isi).await;
-    }
-
-    pub async fn recv(&mut self) -> Option<std::result::Result<proto::Insim, std::io::Error>> {
-        // TODO: This should probably be done with a sink and a stream?
-        let result = self.inner.next().await;
-
-        // TODO remove
-        println!("[recv] {:?}", result);
 
         // TODO implement unexpected version handling
+    }
 
-        // keep the connection alive
-        if let Some(Ok(proto::Insim::Tiny { reqi: 0, .. })) = result {
-            println!("ping? pong!");
-            let pong = proto::Insim::Tiny {
-                reqi: 0,
-                subtype: 0,
-            };
-            self.send(pong).await;
+    pub async fn run(&mut self) {
+        let mut interval = time::interval(Duration::from_secs(15));
+
+        loop {
+            tokio::select! {
+                Some(result) = self.inner.next() => {
+
+                    // TODO move this into it's own handler fn of some kind
+                    match result {
+                        Ok(proto::Insim::Tiny { reqi: 0, .. }) => {
+                            // keep the connection alive
+                            println!("ping? pong!");
+                            let pong = proto::Insim::Tiny {
+                                reqi: 0,
+                                subtype: 0,
+                            };
+                            self.send(pong).await;
+                        },
+
+                        Ok(frame) => {
+                            self.timeout = next_timeout();
+
+                            // TODO remove
+                            println!("[recv] {:?}", frame);
+
+                            // TODO event handling of some kind.
+                            // We could do something like the command macros in
+                            // https://github.com/serenity-rs/serenity?
+                        },
+
+                        // TODO add unknown packet handling to just log an error
+                        // after that, switch this to return
+                        Err(error) => {
+                            println!("[err] {:?}", error);
+                        },
+                    }
+                },
+
+                tick = interval.tick() => {
+                    if tick > self.timeout {
+                        println!("Timeout!");
+                        return
+                    }
+
+                    // TODO remove
+                    println!("TICK {:?}", tick);
+                }
+            }
         }
-
-        result
     }
 
     pub async fn send(&mut self, data: proto::Insim) -> std::result::Result<(), std::io::Error> {
@@ -65,8 +105,4 @@ impl Client {
         println!("[send] {:?}", data);
         self.inner.send(data).await
     }
-
-    // TODO event handling of some kind.
-    // We could do something like the command macros in
-    // https://github.com/serenity-rs/serenity?
 }
