a43efacbd05ca3f3d7ea0cb42cf13205be4205d7
Karl Southern <karl@theangryangel.co.uk>
2022-09-09 11:47:30 +0100

    feat(pth2svg): support drawing multiple pth files as a single track, add configurable colours

--- pth2svg/src/main.rs
+++ pth2svg/src/main.rs
@@ -9,129 +9,129 @@ use std::path;
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
-    input: path::PathBuf,
+    inputs: Vec<path::PathBuf>,
 
     #[clap(short, long)]
     output: path::PathBuf,
+
+    #[clap(short, long, default_value_t = String::from("#3D9970"))]
+    background_colour: String,
+
+    #[clap(short, long, default_value_t = String::from("#111111"))]
+    track_colour: String,
 }
 
 // FIXME - remove the unwraps
 fn main() {
     let args = Args::parse();
 
-    if !args.input.exists() {
-        // FIXME
-        panic!("Could not find input file");
-    }
-
     if args.output.exists() {
         // FIXME
         panic!("output already exists!");
     }
 
-    // FIXME
-    let mut input = fs::File::open(args.input).unwrap();
+    let mut document = svg::Document::new();
 
-    let mut buffer = Vec::new();
-
-    // read the whole file
-    input.read_to_end(&mut buffer).unwrap();
-
-    let p = Pth::try_from(buffer.as_ref()).unwrap();
-
-    //let mut outer = Vec::new();
-    //let mut road = Vec::new();
+    let mut all_pth_nodes = Vec::new();
 
     let mut x: (f32, f32) = (0.0, 0.0);
     let mut y: (f32, f32) = (0.0, 0.0);
 
-    let mut nodes = p.nodes;
-    nodes.insert(0, *nodes.last().unwrap());
+    for i in args.inputs.iter() {
+        if !i.exists() {
+            // FIXME
+            panic!("Could not find input file");
+        }
 
-    let mut document = svg::Document::new();
+        // FIXME
+        let mut input = fs::File::open(i).unwrap();
+        let mut buffer = Vec::new();
 
-    let mut data = svg::node::element::path::Data::new().move_to((
-        nodes.first().unwrap().center.x as f32 / 65536.0,
-        nodes.first().unwrap().center.y as f32 / 65536.0,
-    ));
+        // read the whole file
+        input.read_to_end(&mut buffer).unwrap();
 
-    for pair in nodes.windows(2) {
-        let prev = pair[0];
-        let next = pair[1];
+        let p = Pth::try_from(buffer.as_ref()).unwrap();
 
-        let prev_outer = prev.get_outer_limit(Some(65536.0));
-        let next_outer = next.get_outer_limit(Some(65536.0));
+        // wrap around the nodes
+        let mut nodes = p.nodes.clone();
+        nodes.insert(0, *nodes.last().unwrap());
 
-        data = data.line_to((
-            prev.center.x as f32 / 65536.0,
-            prev.center.y as f32 / 65536.0,
-        ));
+        all_pth_nodes.push(nodes);
+    }
 
-        x.0 = x.0.min(prev_outer.0.x);
-        x.1 = x.1.max(prev_outer.0.x);
+    // draw all the track limits first.
+    for nodes in all_pth_nodes.iter() {
+        for pair in nodes.windows(2) {
+            let prev = pair[0];
+            let next = pair[1];
 
-        x.0 = x.0.min(prev_outer.1.x);
-        x.1 = x.1.max(prev_outer.1.x);
+            let prev_outer = prev.get_outer_limit(Some(65536.0));
+            let next_outer = next.get_outer_limit(Some(65536.0));
 
-        y.0 = y.0.min(prev_outer.0.y);
-        y.1 = y.1.max(prev_outer.0.y);
+            x.0 = x.0.min(prev_outer.0.x);
+            x.1 = x.1.max(prev_outer.0.x);
 
-        y.0 = y.0.min(prev_outer.1.y);
-        y.1 = y.1.max(prev_outer.1.y);
+            x.0 = x.0.min(prev_outer.1.x);
+            x.1 = x.1.max(prev_outer.1.x);
 
-        let poly = svg::node::element::Polygon::new()
-            .set("style", "fill: green")
-            .set(
-                "points",
-                format!(
-                    "{},{} {},{} {},{} {},{}",
-                    prev_outer.0.x,
-                    prev_outer.0.y,
-                    next_outer.0.x,
-                    next_outer.0.y,
-                    next_outer.1.x,
-                    next_outer.1.y,
-                    prev_outer.1.x,
-                    prev_outer.1.y,
-                ),
-            );
+            y.0 = y.0.min(prev_outer.0.y);
+            y.1 = y.1.max(prev_outer.0.y);
 
-        document = document.add(poly);
+            y.0 = y.0.min(prev_outer.1.y);
+            y.1 = y.1.max(prev_outer.1.y);
 
-        let prev_outer = prev.get_road_limit(Some(65536.0));
-        let next_outer = next.get_road_limit(Some(65536.0));
+            let poly = svg::node::element::Polygon::new()
+                .set("style", format!("fill: {}", args.background_colour))
+                .set(
+                    "points",
+                    format!(
+                        "{},{} {},{} {},{} {},{}",
+                        prev_outer.0.x,
+                        prev_outer.0.y,
+                        next_outer.0.x,
+                        next_outer.0.y,
+                        next_outer.1.x,
+                        next_outer.1.y,
+                        prev_outer.1.x,
+                        prev_outer.1.y,
+                    ),
+                );
 
-        let poly = svg::node::element::Polygon::new()
-            .set("style", "fill: black")
-            .set(
-                "points",
-                format!(
-                    "{},{} {},{} {},{} {},{}",
-                    prev_outer.0.x,
-                    prev_outer.0.y,
-                    next_outer.0.x,
-                    next_outer.0.y,
-                    next_outer.1.x,
-                    next_outer.1.y,
-                    prev_outer.1.x,
-                    prev_outer.1.y,
-                ),
-            );
+            document = document.add(poly);
+        }
+    }
 
-        document = document.add(poly);
+    // draw all the roads next
+    for nodes in all_pth_nodes.iter() {
+        for pair in nodes.windows(2) {
+            let prev = pair[0];
+            let next = pair[1];
+
+            let prev_outer = prev.get_road_limit(Some(65536.0));
+            let next_outer = next.get_road_limit(Some(65536.0));
+
+            let poly = svg::node::element::Polygon::new()
+                .set("style", format!("fill: {}", args.track_colour))
+                .set(
+                    "points",
+                    format!(
+                        "{},{} {},{} {},{} {},{}",
+                        prev_outer.0.x,
+                        prev_outer.0.y,
+                        next_outer.0.x,
+                        next_outer.0.y,
+                        next_outer.1.x,
+                        next_outer.1.y,
+                        prev_outer.1.x,
+                        prev_outer.1.y,
+                    ),
+                );
+
+            document = document.add(poly);
+        }
     }
 
     document = document.set("viewBox", (x.0, y.0, x.1 - x.0, y.1 - y.0));
-
-    data = data.close();
-
-    let path = svg::node::element::Path::new()
-        .set("fill", "none")
-        .set("stroke", "red")
-        .set("stroke-width", 1)
-        .set("d", data);
-
-    document = document.add(path);
 
     svg::save(args.output, &document).unwrap();
 }
