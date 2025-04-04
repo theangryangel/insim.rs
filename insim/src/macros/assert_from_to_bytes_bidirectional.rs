36ee41fea7e6dec49f42d27c6ec4e7e6ae3c86d3
Karl <karl@theangryangel.co.uk>
2025-04-04 23:33:18 +0100

    Update assert_from_to_bytes_bidirectional.rs

--- insim/src/macros/assert_from_to_bytes_bidirectional.rs
+++ insim/src/macros/assert_from_to_bytes_bidirectional.rs
@@ -6,18 +6,19 @@ macro_rules! assert_from_to_bytes_bidirectional {
         let parsed_binrw = $thing::read_le(&mut ::std::io::Cursor::new(&raw)).unwrap();
         let mut written_binrw = ::std::io::Cursor::new(Vec::new());
         parsed_binrw.write_le(&mut written_binrw).unwrap();
-        assert_eq!(raw, written_binrw.into_inner());
+        assert_eq!(raw, written_binrw.into_inner(), "assert binrw reads and writes.");
 
         // test FromToBytes
         let mut parsed_buf = ::bytes::BytesMut::new();
         parsed_buf.extend_from_slice(&raw);
 
         let parsed = $thing::from_bytes(&mut parsed_buf.freeze()).unwrap();
+        assert_eq!(buf.remaining(), 0);
 
         let mut written_buf = ::bytes::BytesMut::new();
         parsed.to_bytes(&mut written_buf).unwrap();
 
-        assert_eq!(&raw, written_buf.as_ref());
+        assert_eq!(&raw, written_buf.as_ref(), "assert fromtobytes matches");
 
         parsed
     }};
