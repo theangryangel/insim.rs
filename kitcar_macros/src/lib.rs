647dd2adcaa62a691d0a9594d441092966bb5935
Karl <karl@theangryangel.co.uk>
2026-01-24 22:44:41 +0000

    refactor(kitcar_macros): use darling (#295)
    Fixes #285

--- kitcar_macros/src/lib.rs
+++ kitcar_macros/src/lib.rs
@@ -1,56 +1,131 @@
 //! Macros for the kitcar crate
+use darling::{FromDeriveInput, FromField, FromVariant, ast};
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Data, DeriveInput, Fields, parse_macro_input};
+use syn::{Attribute, DeriveInput, parse_macro_input};
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(chat), supports(enum_any))]
+struct ChatInput {
+    ident: syn::Ident,
+    data: ast::Data<ChatVariant, ()>,
+    #[darling(default)]
+    prefix: Option<char>,
+}
+
+#[derive(Debug, FromVariant)]
+#[darling(forward_attrs(doc))]
+struct ChatVariant {
+    ident: syn::Ident,
+    fields: ast::Fields<ChatField>,
+    attrs: Vec<Attribute>,
+}
+
+impl ChatVariant {
+    fn doc_string(&self) -> Option<String> {
+        let docs: Vec<String> = self
+            .attrs
+            .iter()
+            .filter_map(|attr| {
+                if attr.path().is_ident("doc")
+                    && let syn::Meta::NameValue(meta) = &attr.meta
+                    && let syn::Expr::Lit(expr_lit) = &meta.value
+                    && let syn::Lit::Str(lit_str) = &expr_lit.lit
+                {
+                    return Some(lit_str.value().trim().to_string());
+                }
+                None
+            })
+            .collect();
+
+        if docs.is_empty() {
+            None
+        } else {
+            Some(docs.join(" "))
+        }
+    }
+}
+
+#[derive(Debug, FromField)]
+struct ChatField {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+}
+
+impl ChatField {
+    fn is_option(&self) -> bool {
+        if let syn::Type::Path(type_path) = &self.ty
+            && let Some(segment) = type_path.path.segments.last()
+        {
+            return segment.ident == "Option";
+        }
+        false
+    }
+
+    fn option_inner(&self) -> &syn::Type {
+        if let syn::Type::Path(type_path) = &self.ty
+            && let Some(segment) = type_path.path.segments.last()
+            && segment.ident == "Option"
+            && let syn::PathArguments::AngleBracketed(args) = &segment.arguments
+            && let Some(syn::GenericArgument::Type(inner)) = args.args.first()
+        {
+            return inner;
+        }
+        &self.ty
+    }
+}
 
 /// Convert an enum to something that impl a parse and parse_with_prefix function to handle chat
 /// messages
 #[proc_macro_derive(ParseChat, attributes(chat))]
 pub fn derive_command_parser(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let enum_name = &input.ident;
 
-    let Data::Enum(data_enum) = &input.data else {
-        panic!("CommandParser can only be derived for enums");
+    let chat_input = match ChatInput::from_derive_input(&input) {
+        Ok(v) => v,
+        Err(e) => return e.write_errors().into(),
     };
 
-    let prefix = extract_prefix(&input.attrs);
+    let enum_name = &chat_input.ident;
+    let prefix = chat_input.prefix;
+
+    let variants = chat_input
+        .data
+        .take_enum()
+        .expect("ChatInput only supports enums");
 
     let mut match_arms = vec![];
     let mut help_entries = vec![];
 
-    for variant in &data_enum.variants {
+    for variant in &variants {
         let variant_name = &variant.ident;
         let cmd_name = variant_name.to_string().to_lowercase();
+        let doc_string = variant.doc_string();
 
-        let doc_string = extract_doc_comments(&variant.attrs);
+        match &variant.fields.style {
+            ast::Style::Struct => {
+                let fields: Vec<_> = variant.fields.iter().collect();
 
-        match &variant.fields {
-            Fields::Named(fields) => {
                 let field_names: Vec<_> = fields
-                    .named
                     .iter()
                     .map(|f| f.ident.as_ref().expect("Missing field name"))
                     .collect();
 
-                let field_types: Vec<_> = fields.named.iter().map(|f| &f.ty).collect();
+                let required_fields: Vec<_> = fields.iter().filter(|f| !f.is_option()).collect();
 
-                let required_fields: Vec<_> = field_names
+                let optional_fields: Vec<_> = fields
                     .iter()
-                    .zip(field_types.iter())
-                    .filter(|(_, ty)| !is_option_type(ty))
-                    .collect();
-
-                let optional_fields: Vec<_> = field_names
-                    .iter()
-                    .zip(field_types.iter())
                     .enumerate()
-                    .filter(|(_, (_, ty))| is_option_type(ty))
+                    .filter(|(_, f)| f.is_option())
                     .collect();
 
-                let parse_required: Vec<_> = required_fields.iter().enumerate()
-                    .map(|(i, (name, ty))| {
+                let parse_required: Vec<_> = required_fields
+                    .iter()
+                    .enumerate()
+                    .map(|(i, f)| {
+                        let name = f.ident.as_ref().unwrap();
                         let name_str = name.to_string();
+                        let ty = &f.ty;
                         quote! {
                             let #name = args.get(#i)
                                 .ok_or_else(|| kitcar::chat::ParseError::MissingRequiredArg(#name_str.to_string()))?;
@@ -60,10 +135,11 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
                     })
                     .collect();
 
-                let parse_optional: Vec<_> = optional_fields.iter()
-                    .map(|(i, (name, ty))| {
-                        // Extract T from Option<T>
-                        let inner_ty = extract_option_inner(ty);
+                let parse_optional: Vec<_> = optional_fields
+                    .iter()
+                    .map(|(i, f)| {
+                        let name = f.ident.as_ref().unwrap();
+                        let inner_ty = f.option_inner();
                         quote! {
                             let #name = args.get(#i)
                                 .map(|s| <#inner_ty as kitcar::chat::FromArg>::from_arg(s))
@@ -73,14 +149,13 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
                     })
                     .collect();
 
-                // Generate help text
                 let required_args: Vec<_> = required_fields
                     .iter()
-                    .map(|(name, _)| format!("<{}>", name))
+                    .map(|f| format!("<{}>", f.ident.as_ref().unwrap()))
                     .collect();
                 let optional_args: Vec<_> = optional_fields
                     .iter()
-                    .map(|(_, (name, _))| format!("[{}]", name))
+                    .map(|(_, f)| format!("[{}]", f.ident.as_ref().unwrap()))
                     .collect();
                 let all_args = [required_args, optional_args].concat();
                 let args_str = all_args.join(" ");
@@ -97,9 +172,7 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
                     format!(" - {}{} {}", prefix.unwrap_or(' '), cmd_name, args_str)
                 };
 
-                help_entries.push(quote! {
-                    #help_str
-                });
+                help_entries.push(quote! { #help_str });
 
                 match_arms.push(quote! {
                     #cmd_name => {
@@ -109,19 +182,22 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
                     }
                 });
             },
-            Fields::Unnamed(_) => {
-                panic!("CommandParser does not support tuple variants");
+            ast::Style::Tuple => {
+                return syn::Error::new_spanned(
+                    variant_name,
+                    "ParseChat does not support tuple variants",
+                )
+                .to_compile_error()
+                .into();
             },
-            Fields::Unit => {
+            ast::Style::Unit => {
                 let help_str = if let Some(doc) = doc_string {
                     format!(" - {}{} - {}", prefix.unwrap_or(' '), cmd_name, doc)
                 } else {
                     format!(" - {}{}", prefix.unwrap_or(' '), cmd_name)
                 };
 
-                help_entries.push(quote! {
-                    #help_str
-                });
+                help_entries.push(quote! { #help_str });
 
                 match_arms.push(quote! {
                     #cmd_name => Ok(#enum_name::#variant_name)
@@ -130,7 +206,7 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
         }
     }
 
-    let strip = if let Some(prefix_char) = prefix.as_ref() {
+    let strip = if let Some(prefix_char) = prefix {
         quote! {
             if let Some(stripped) = input.strip_prefix(#prefix_char) {
                 stripped
@@ -139,9 +215,7 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
             }
         }
     } else {
-        quote! {
-            input
-        }
+        quote! { input }
     };
 
     let prefix_return = match prefix {
@@ -159,7 +233,6 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
                     return Err(kitcar::chat::ParseError::EmptyInput);
                 }
 
-                // Strip prefix if required
                 let input = #strip;
 
                 let parts: Vec<&str> = input.split_whitespace().collect();
@@ -198,76 +271,4 @@ pub fn derive_command_parser(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
-}
-
-fn is_option_type(ty: &syn::Type) -> bool {
-    if let syn::Type::Path(type_path) = ty
-        && let Some(segment) = type_path.path.segments.last()
-    {
-        return segment.ident == "Option";
-    }
-    false
-}
-
-fn extract_option_inner(ty: &syn::Type) -> &syn::Type {
-    if let syn::Type::Path(type_path) = ty
-        && let Some(segment) = type_path.path.segments.last()
-        && segment.ident == "Option"
-        && let syn::PathArguments::AngleBracketed(args) = &segment.arguments
-        && let Some(syn::GenericArgument::Type(inner)) = args.args.first()
-    {
-        return inner;
-    }
-    ty
-}
-
-fn extract_doc_comments(attrs: &[syn::Attribute]) -> Option<String> {
-    let docs: Vec<String> = attrs
-        .iter()
-        .filter_map(|attr| {
-            if attr.path().is_ident("doc")
-                && let syn::Meta::NameValue(meta) = &attr.meta
-                && let syn::Expr::Lit(expr_lit) = &meta.value
-                && let syn::Lit::Str(lit_str) = &expr_lit.lit
-            {
-                return Some(lit_str.value().trim().to_string());
-            }
-            None
-        })
-        .collect();
-
-    if docs.is_empty() {
-        None
-    } else {
-        Some(docs.join(" "))
-    }
-}
-
-fn extract_prefix(attrs: &[syn::Attribute]) -> Option<char> {
-    for attr in attrs {
-        if attr.path().is_ident("chat")
-            && let Ok(meta_list) = attr.meta.require_list()
-            && let Ok(nested_metas) = meta_list.parse_args_with(
-                syn::punctuated::Punctuated::<syn::Meta, syn::Token![,]>::parse_terminated,
-            )
-        {
-            for nested in nested_metas {
-                if let syn::Meta::NameValue(nv) = nested
-                    && nv.path.is_ident("prefix")
-                    && let syn::Expr::Lit(expr_lit) = &nv.value
-                {
-                    if let syn::Lit::Char(lit_char) = &expr_lit.lit {
-                        return Some(lit_char.value());
-                    }
-                    if let syn::Lit::Str(lit_str) = &expr_lit.lit {
-                        let s = lit_str.value();
-                        if s.len() == 1 {
-                            return s.chars().next();
-                        }
-                    }
-                }
-            }
-        }
-    }
-    None
 }
