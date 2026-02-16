ee578fddd269396d3764a0f34828ff1e5821e8f9
Karl Southern <karl@theangryangel.co.uk>
2026-02-16 15:08:09 +0000

    fix(insim): allow builder to explicitly set the udpport, fixes #339

---
+++ examples/ssg-manual/src/main.rs
@@ -0,0 +1,104 @@
+//! High level example of using a combination of TCP and UDP.
+use std::{net::SocketAddr, time::Duration};
+
+use bytes::{Buf, Bytes, BytesMut};
+use clap::Parser;
+use insim::{core::Decode, insim::SmallType, Packet};
+use outgauge::Outgauge;
+use outsim::OutsimPack;
+use tokio::net::UdpSocket;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[arg(long)]
+    /// host:port of LFS to connect to
+    addr: SocketAddr,
+
+    #[arg(long)]
+    /// host:port to bind a udp listener to.
+    udp_listen_addr: SocketAddr,
+}
+
+fn setup_tracing_subscriber() {
+    // Setup with a default log level of INFO RUST_LOG is unset
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+}
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    // Setup tracing_subcriber with some sane defaults
+    setup_tracing_subscriber();
+
+    // Parse our command line arguments, using clap
+    let cli = Cli::parse();
+
+    let udp = UdpSocket::bind(cli.udp_listen_addr).await?;
+    // Make a buffer for the UdpSocket, we'll reuse this across recv_from calls
+    let mut buf = [0; 1024];
+
+    // Make our TCP connection
+    let mut connection = insim::tcp(cli.addr)
+        .isi_flag_mci(true)
+        .isi_flag_con(true)
+        .isi_flag_obh(true)
+        .isi_flag_axm_load(true)
+        .isi_flag_axm_edit(true)
+        .isi_interval(Duration::from_secs(1))
+        // Force the UDP port, so that it's sent to our socket
+        .isi_udpport(cli.udp_listen_addr.port())
+        .connect_async()
+        .await?;
+
+    // Establish a connection
+    tracing::info!("Connected!");
+
+    // Start sending gauges (outgauge)
+    connection
+        .write(SmallType::Ssg(Duration::from_secs(1)))
+        .await?;
+
+    // Start sending positions (outsim)
+    connection
+        .write(SmallType::Ssp(Duration::from_secs(1)))
+        .await?;
+
+    loop {
+        tokio::select! {
+            packet = connection.read() => {
+                let packet = packet?;
+                tracing::info!("{:?}", packet);
+            },
+            res = udp.recv_from(&mut buf) => {
+                let (amt, src) = res?;
+                let mut buf: Bytes = BytesMut::from(&buf[..amt]).freeze();
+
+                if amt == 92 {
+                    // Conventionally it's outgauge
+                    let packet = Outgauge::decode(&mut buf)?;
+                    tracing::info!("outgauge: from={:?}, data={:?}", src, packet);
+                } else if amt == 64 {
+                    // Conventionally it's outsim
+                    let packet = OutsimPack::decode(&mut buf)?;
+                    tracing::info!("outsim: from={:?}, data={:?}", src, packet);
+                } else if amt > 1 {
+                    // Otherwise it's probably a Mci or Nlp packet
+                    //
+                    // XXX: we need to chop off the first byte, which is the length of the packet.
+                    // That's normally handled internally
+                    // by the insim crate.
+                    buf.advance(1);
+                    let packet = Packet::decode(&mut buf)?;
+                    tracing::info!("insim: from={:?}, data={:?}", src, packet);
+                }
+            }
+        }
+    }
+}
--- insim/src/builder.rs
+++ insim/src/builder.rs
@@ -58,6 +58,7 @@ pub struct Builder {
     isi_interval: Option<Duration>,
     isi_iname: Option<String>,
     isi_reqi: RequestId,
+    isi_udpport: Option<u16>,
 
     // Choosing to use separate fields with a prefix, rather than an enum because when this was
     // originally implemented it supported LFSW Relay, which no longer exists, and if you were to do
@@ -89,6 +90,7 @@ impl Default for Builder {
             isi_iname: None,
             isi_interval: None,
             isi_reqi: RequestId(0),
+            isi_udpport: None,
         }
     }
 }
@@ -252,11 +254,31 @@ impl Builder {
         self
     }
 
+    /// Explicitly set the udpport to be used in the [crate::Packet::Isi] packet during connection
+    /// handshake.
+    ///
+    /// If you explicitly set this then it will override any values derived from the `udp_local_address`.
+    /// You may wish to do this in one of 2 scenarios:
+    /// - NAT/PAT (read: port forwarding) reasons
+    /// - You want to receive positional and other information over UDP, whilst using TCP.
+    ///
+    /// If you choose to use a combination of Tcp and Udp (insim protocol dictates that if a udpport is provided
+    /// then [crate::Packet::Mci], [crate::Packet::Nlp], Outgauge and Outsim packets will be sent to the
+    /// udpport) be aware that this library *does not* currently automatically handle this protocol split for
+    /// you. You **must** spawn the relevant UdpSocket by hand. You can find an example of this in
+    /// the examples/ssg-manual/ directory.
+    ///
+    /// This may change in future versions.
+    pub fn isi_udpport<P: Into<Option<u16>>>(mut self, udpport: P) -> Self {
+        self.isi_udpport = udpport.into();
+        self
+    }
+
     /// Create a [crate::insim::Isi] from this configuration.
     pub fn isi(&self) -> Isi {
         let udpport = match self.proto {
             Proto::Udp => self.udp_local_address.unwrap().port(),
-            _ => 0,
+            Proto::Tcp => self.isi_udpport.unwrap_or(0),
         };
 
         Isi {
